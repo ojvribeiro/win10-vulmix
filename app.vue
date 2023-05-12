4aeea2d80216c20231b26b61725f848866bced53
Victor Ribeiro <devjvribeiro@gmail.com>
2023-05-12 01:54:41 -0300

    feat: Wallpaper component

--- app.vue
+++ app.vue
@@ -1,4 +1,5 @@
 <template>
+  <Wallpaper />
 </template>
 
 <style>
---
+++ components/Wallpaper.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="h-screen w-screen bg-black">
+    <img
+      src="/assets/img/wallpaper.jpg"
+      class="h-full w-full object-cover"
+      alt=""
+    />
+  </div>
+</template>
