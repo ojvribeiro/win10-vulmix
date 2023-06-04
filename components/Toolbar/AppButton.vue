c1104a848de6229a5aed2d293f9fb8e6ba441b6b
Victor Ribeiro <devjvribeiro@gmail.com>
2023-06-04 12:44:57 -0300

    fix: better easing for taskbar buttons

--- components/Toolbar/AppButton.vue
+++ components/Toolbar/AppButton.vue
@@ -1,9 +1,19 @@
 <template>
   <button
-    class="block h-full w-12 cursor-default text-white transition-colors duration-100 hover:bg-white/10 active:bg-white/5"
+    class="block h-full w-12 cursor-default text-white hover:bg-white/10 active:bg-white/5"
   >
     <div class="flex h-full w-full items-center justify-center">
       <slot />
     </div>
   </button>
 </template>
+
+<style scoped lang="scss">
+  button {
+    transition: 0.2s background-color ease-out;
+
+    &:hover {
+      transition: 0.05s background-color ease-in;
+    }
+  }
+</style>
--- components/Toolbar/TaskbarButton.vue
+++ components/Toolbar/TaskbarButton.vue
@@ -1,9 +1,19 @@
 <template>
   <button
-    class="block h-full w-fit cursor-default text-white transition-colors duration-75 hover:bg-white/10 active:bg-white/20"
+    class="block h-full w-fit cursor-default text-white hover:bg-white/10 active:bg-white/20"
   >
     <div class="flex h-full w-full items-center justify-center">
       <slot />
     </div>
   </button>
 </template>
+
+<style scoped lang="scss">
+  button {
+    transition: 0.12s background-color ease-out;
+
+    &:hover {
+      transition: 0.02s background-color ease-in;
+    }
+  }
+</style>
