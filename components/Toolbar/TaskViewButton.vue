d4175f27f07bbf786eab00f7832b6163faf4b45e
Victor Ribeiro <devjvribeiro@gmail.com>
2023-06-04 12:51:01 -0300

    fix: prevent flickering on hovering the TaskView button
    The flickering was caused by the second icon (which only appears on hover) being loaded late.

--- components/Toolbar/TaskViewButton.vue
+++ components/Toolbar/TaskViewButton.vue
@@ -3,10 +3,27 @@
     @mouseover="taskViewState = '2'"
     @mouseout="taskViewState = '1'"
   >
-    <Icon :name="`task-view-${taskViewState}`" />
+    <Icon
+      v-for="i in 2"
+      :name="`task-view-${i}`"
+      :class="[
+        taskViewState === i.toString() ? 'opacity-100' : 'opacity-0',
+
+        `item-${i}`,
+      ]"
+    />
   </ToolbarSystemButton>
 </template>
 
-<script setup>
-  const taskViewState = ref('1')
+<script setup lang="ts">
+  const taskViewState = ref<string>('1')
 </script>
+
+<style scoped>
+  .item-1 {
+    transform: translateX(50%);
+  }
+  .item-2 {
+    transform: translateX(-50%);
+  }
+</style>
